7f8b9036b1d1e0ee2b69893fff8fc3fa0c7844db
Swaggero_o <mugel849@gmail.com>
2022-02-14 13:34:03 +0100

    ini

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Shop</title>
+</head>
+<body>
+    <h1>Shop</h1>
+    <a href="./buy.html">BUY</a>
+</body>
+</html>
