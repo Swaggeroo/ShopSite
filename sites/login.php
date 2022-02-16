01f59a32db7fc6285a4aaa1afafdef3ad1481a6d
Swaggero_o <mugel849@gmail.com>
2022-02-16 10:33:31 +0100

    navbar sticky

---
+++ sites/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <?php require "../php/#navBar.php" ?>
+</body>
+</html>
