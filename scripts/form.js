3bb406cd8cfc79dd04cbc85cc79c0500c6064167
Swaggeroo <mugel849@gmail.com>
2022-02-17 21:07:18 +0100

    Form Autofill Colors

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,15 @@
 let inputs = document.getElementsByClassName("input");
 
 for(let i = 0; i<inputs.length; i++){
-    inputs[i].addEventListener("focusout", (event)=> {
-        if (event.target.checkValidity()){
+    inputs[i].addEventListener("focusout", checkVal);
+
+    inputs[i].addEventListener("change", checkVal);
+
+    function checkVal(event) {
+        if (event.target.checkValidity()) {
             event.target.classList.remove("invalid");
-        }else{
+        } else {
             event.target.classList.add("invalid");
         }
-    });
+    }
 }
