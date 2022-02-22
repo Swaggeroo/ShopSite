84faf9d17798668fd06bd90ceda85bbc44cb2bf3
Swaggeroo <mugel849@gmail.com>
2022-02-22 19:43:44 +0100

    Form PLZ Pattern

--- scripts/form.js
+++ scripts/form.js
@@ -13,3 +13,12 @@ for(let i = 0; i<inputs.length; i++){
         }
     }
 }
+
+const plzObj = document.getElementById("plz");
+plzObj.addEventListener("input", (e)=>{
+    let s = e.target.value
+    let last = s.slice(-1)
+    if (isNaN(last) && !Number.isInteger(parseFloat(last))){
+        plzObj.value = s.substring(0,s.length-1)
+    }
+})
