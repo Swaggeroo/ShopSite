b9a84ad93baf531871247c3247d5677dbf8b1957
Swaggeroo <mugel849@gmail.com>
2022-03-10 20:22:57 +0100

    Bill Template start

--- sites/rechnungDrucken.php
+++ sites/rechnungDrucken.php
@@ -1,2 +1,25 @@
 <?php
-echo $_GET["id"];
+//echo $_GET["id"];?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Rechnung Drucken</title>
+    <link rel="stylesheet" href="../css/bill.css">
+    <link rel="icon" href="../media/icons/favicon.SVG" sizes="any">
+</head>
+<body>
+    <img src="../media/icons/faviconDARK.SVG">
+    <script>
+        self.print();
+        if (window.matchMedia) {
+            var mediaQueryList = window.matchMedia('print');
+            mediaQueryList.addListener(function(mql) {
+                if (!mql.matches){
+                    window.close();
+                }
+            });
+        }
+    </script>
+</body>
+</html>
