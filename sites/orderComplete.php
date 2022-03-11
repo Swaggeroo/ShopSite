75dcdbc17ac68a322004004364b41fa9c404db84
Swaggeroo <mugel849@gmail.com>
2022-03-11 20:04:48 +0100

    Rechnung in neuen Fenster

--- sites/orderComplete.php
+++ sites/orderComplete.php
@@ -25,7 +25,7 @@
             </div>
         </div>
         <p style="margin-top: 90px"><a href="./shop.php">Weiter Shoppen</a></p>
-        <p><a href="./rechnungDrucken.php?id=<?php echo $_GET["id"]?>">Rechnung drucken</a></p>
+        <p><a href="./rechnungDrucken.php?id=<?php echo $_GET["id"]?>" target="_blank">Rechnung drucken</a></p>
     </div>
 </body>
 </html>
