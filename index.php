8be7e230d05ee06f131a2aa7b1bb4fd6696eb35f
Swaggeroo <mugel849@gmail.com>
2022-03-25 20:36:25 +0100

    new home Button

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Home</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="./css/buttonWhite.css">
     <link rel="icon" href="./media/icons/favicon.SVG" sizes="any">
     <script src="./scripts/home.js" defer></script>
 </head>
@@ -16,9 +17,17 @@
     <?php require "./php/#navBar.php"?>
     <div class="banner">
         <div id="textbox">
-            <h1>Tiertotal</h1>
+            <h1 style="padding-top: .1em">Tiertotal</h1>
             <h2>Von klein bis groß - Der Shop für alle Tier Bedürfnisse</h2>
-            <h3><a class="toShopLink" href="./sites/shop.php">Zum Shop</a></h3>
+            <div class='fancy-btn-cont' style="margin-bottom: .5em; margin-top: 2em">
+                <a class='fancy-btn' href="./sites/shop.php" style="font-weight: bold">
+                    Zum Shop
+                    <span class='line-1'></span>
+                    <span class='line-2'></span>
+                    <span class='line-3'></span>
+                    <span class='line-4'></span>
+                </a>
+            </div>
         </div>
     </div>
 </body>
